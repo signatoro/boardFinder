0031ec07d4ae49474299ee5a2f2d2718bd73fb43
rishavsarma5 <rishavsarma5@gmail.com>
2023-11-17 17:14:08 -0500

    changed code structure to use kv files
    note: create_account logic not converted to work rn

--- app/main.py
+++ app/main.py
@@ -1,42 +1,59 @@
-from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.boxlayout import BoxLayout
+from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen, NoTransition, CardTransition
 from kivy.core.window import Window
-
-from home import HomeScreen
-from createAccount import CreateAccountScreen
+from kivymd.app import MDApp
+import kivy.utils
 
 fixed_size = (Window.size[1] * 0.66 * 1, Window.size[1] * 1)
 
 
+class HomeScreen(Screen):
+    pass
+
+
+class CreateAGroupScreen(Screen):
+    pass
+
+
+# Set minimum window size for desktop and mobile
+Window.minimum_width = fixed_size[0]
+Window.minimum_height = fixed_size[1]
+Window.size = fixed_size
+
+
 # The main application
-class MyApp(App):
+class MyApp(MDApp):
+    fixed_size = (Window.size[1] * 0.66 * 1, Window.size[1] * 1)
     def build(self):
-        # Set minimum window size for desktop and mobile
-        Window.minimum_width = fixed_size[0]
-        Window.minimum_height = fixed_size[1]
-        Window.size = fixed_size
-
-        # returns a window object with all its widgets
-        self.window = BoxLayout(orientation='vertical')
-        self.window.cols = 1
-        self.window.size_hint = (None, None)  # Disable automatic size_hint
-        self.window.width = fixed_size[0]  # Set an initial width
-        self.window.height = fixed_size[1]  # Set an initial height
-        self.window.pos_hint = {"center_x": 0.5, "center_y": 0.5}
-
-        # Set up screen manager and all screens
-        screen_manager = ScreenManager()
-        screen_manager.add_widget(HomeScreen(name="home"))
-        screen_manager.add_widget(CreateAccountScreen(name="create_account"))
-        return screen_manager
+        Window.bind(on_resize=self.reSize)
+        self.title = 'BoardGame Group Finder'
+        self.theme_cls.primary_palette = "Teal"
+        return Builder.load_file("main.kv")  # GUI
 
     def reSize(*args):
         # fixed_size = (Window.size[1] * 0.66, Window.size[1])
         Window.size = fixed_size
         return True
 
-    Window.bind(on_resize=reSize)
+
+
+    def change_screen(self, screen_name, direction='left', mode=""):
+        # Get the screen manager from the kv file
+        screen_manager = self.root.ids['screen_manager']
+        # print(direction, mode)
+        # If going left, change the transition. Else make lleft the default
+        if direction == 'left':
+            mode = "push"
+        elif direction == 'right':
+            mode = 'pop'
+        elif direction == "None":
+            screen_manager.transition = NoTransition()
+            screen_manager.current = screen_name
+            return
+
+        screen_manager.transition = CardTransition(direction=direction, mode=mode)
+
+        screen_manager.current = screen_name
 
 
 if __name__ == "__main__":
