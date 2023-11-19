ca5fdea8fd14abb5fe040f05ece3a40862ec44aa
Scott Pignataro <81174857+signatoro@users.noreply.github.com>
2023-11-18 20:49:24 -0500

    Working Second Page

--- app/src/gameCard.py
+++ app/src/gameCard.py
@@ -7,6 +7,10 @@ class GameCard(MDCard):
 
     title = StringProperty()
 
+    def on_card_click(self):
+        # if self.collide_point(*touch.pos):
+            # Perform action when the card is clicked
+        print("Card Clicked!")
 
 
 # class PostCard(MDCard):
