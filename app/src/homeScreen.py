d67a6bb6d9771918a37e34b17c22d1e58b864f6f
Scott Pignataro <81174857+signatoro@users.noreply.github.com>
2023-11-20 02:05:19 -0500

    Started cards

--- app/src/homeScreen.py
+++ app/src/homeScreen.py
@@ -1,7 +1,11 @@
-from kivy.uix.screenmanager import Screen
+from datetime import time
 
+from kivy.clock import Clock
 from kivymd.uix.tab import MDTabsBase
+from kivy.uix.screenmanager import Screen
 from kivymd.uix.floatlayout import MDFloatLayout
+
+from src.localEventCard import LocalEventCard
 
 
 class Tab(MDFloatLayout, MDTabsBase):
@@ -12,10 +16,29 @@ class HomeScreen(Screen):
     def __init__(self, **kwargs):
         super(HomeScreen, self).__init__(**kwargs)
 
+    def on_enter(self, *args):
+        Clock.schedule_once(self.load_depends)
+        return super().on_enter(*args)
+
     def load_depends(self, load_deps=None):
+        print("Loading Depends")
         #TODO: Call endpoint get list of Local events
-        local_event_l: list = {}
-        pass
+        local_event1 = LocalEventCard(title= "Swords and Coffee",
+            event_link= "link.url.here",
+            description= "The error message ImportError: cannot import name TimeProperty means that Kivycannot find the TimeProperty class in the kivy.properties module. This can happen for a few reasons:",
+            location_type= "In Person",
+            month= '12',
+            day= '4',
+            time= "1:30 pm",
+            location= "Library, Boston MA"
+        )
+        local_event_l: list = [local_event1]
+
+
+        # adds widgets 
+        for event in local_event_l:
+            self.ids.local_event_carou.add_widget(event)
+        
 
 
     def refresh_local_events(self):
--- app/src/localEventCard.py
+++ app/src/localEventCard.py
@@ -1,8 +1,32 @@
 
 from kivymd.uix.card import MDCard
-from kivy.properties import StringProperty, ListProperty, IntProperty
+from kivy.properties import StringProperty, ListProperty, NumericProperty
 
 
+MONTHS = {
+    1: "Jan",
+    2: "Feb",
+    3: "Mar",
+    4: "Apr",
+    5: "May",
+    6: "Jun",
+    7: "Jul",
+    8: "Aug",
+    9: "Sep",
+    10: "Oct",
+    11: "Nov",
+    12: "Dec"
+}
+
+DAYS_OF_WEEK = {
+    0: "Monday",
+    1: "Tuesday",
+    2: "Wednesday",
+    3: "Thursday",
+    4: "Friday",
+    5: "Saturday",
+    6: "Sunday",
+}
 
 class LocalEventCard(MDCard):
 
@@ -12,14 +36,21 @@ class LocalEventCard(MDCard):
 
     location_type= StringProperty()
 
-    month: IntProperty()
-    day: int
+    month= NumericProperty()
+    day= NumericProperty()
 
-    # TODO: figure out how to do this.
-    # Time 7:00pm or 11:00am
-    time: IntProperty()
+    time= StringProperty()
     location= StringProperty()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
     def load_depends(self, load_deps=None):
         pass
-    pass
+
+    def get_month(self, month_n: int):
+        return MONTHS[month_n]
+    
+    def get_day_of_week(self, day_n:int):
+        return DAYS_OF_WEEK[day_n]
+    
